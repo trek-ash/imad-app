36ee4361547d0be4d3b7d421d8a8781ff75d45eb
trek-ash <yashsinghgreat@gmail.com>
2018-02-15 23:55:33 +0530

    Adding portfolio

---
+++ portfolio/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Personal Portfolio</title>
+	<link href="https://fonts.googleapis.com/css?family=Courgette|Diplomata|Monofett|Modak|Germania+One|Slabo+27px|Open+Sans+27px|Rammetto+One" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="he.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	
+</head>
+<body style="overflow-x: hidden;" >
+
+<div class="head1">
+	<div class="head-text">
+
+		<h1 style="font-family: 'Rammetto One', cursive;">Hello! I'm Yash</h1>
+
+		<h1><p>A <span id="move1"></span><span class="anim">|</span></p></h1>
+	</div>
+	<button class="about btn">
+		<b>About Me</b>
+	</button>
+</div>
+
+<div class="row" style="background-color: #E0E0E0" >
+	<div class="col-md-1 Sidebar"></div>
+	<div class="col-md-10 content">
+		
+		<br><br>
+		<div class="card container-fluid">
+			<h2 style="font-family: 'Rammetto One', cursive;">About Me</h2><hr style="border: none;height: 2px;color:grey;background-color:grey;"></h2>
+			<br>
+			<div class="media-object">
+				<div class="media-left img-responsive" style="height: 7px;width: 7px;"><img src="img/yash.jpg" style="border-radius: 200px"></div>
+				<div class="media-body" style="text-align: justify;">
+					<center><h3 class="media-heading">Yash Singh|<small>Web Designer and Developer</small></h3></center>
+					<hr>
+					<p >
+					I am a Dreamer,Learner and an aspiring Web Designer and Developer.I love working on projects based on web Designing and Development.
+					I started learnig with basics of <b>HTML,CSS</b> continued to learn <b>Javascript</b> to add on my skills of Web 
+					designing.To add on my responsive web-designing abilities,I added <b>Bootstrap</b> to my skills.To add on to my development skills,I learned <b>PHP</b>.<br>
+					Apart from these,I have knowledge of languages like <b>C,Java,Pyhton</b>. 
+					</p>
+					<p >
+					I enjoy watching Sci-fi,Murder Mystery and Thriller Movies and various Series,<b>Game of thrones</b> being on of the favourites. 
+					</p>
+					<h4><i><b> Want to work together?I'd love to hear from you.</b></i></h4>
+				</div>
+			</div>
+
+		</div>
+		<br><br><br>
+		<div class="card" style="background-color: #424242;color: white;text-align: justify;">
+			<div class="row">
+			<div class="col-md-2 " style="top: 0px;"><br><h2>Projects</h2></div>
+			<div class="col-md-10" style="border-left: 2px solid white">
+				<br>
+				<a href="http://www.yashsinghblog.wordpress.com">www.yashsinghblog.wordpress.com</a><br>
+				<a href="http://www.pnwsociety.in">www.pnwsociety.in</a><br>
+				<a href="#">www.bloggersyarn.in</a><br>
+
+				<br><br><br>
+			</div>
+			</div>
+		</div>
+		<br><br><br>
+
+		<div class="card container-fluid">
+			<h2 style="font-family: 'Rammetto One', cursive;">My Resume</h2><hr style="border: none;height: 2px;color:grey;background-color:grey;"></h2>
+			<br>
+			<div class="row">
+				<div class="col-md-6" style="text-align: justify;border-right: 2px solid grey">
+				<h3><b><u>Qualifications:</u></b></h3>
+
+				<p>
+					<b>B.Tech:</b>
+					<br>
+					Pursuing(2016-20)<br>
+					ABES Engineering College,Ghaziabad
+					<br>
+					<b>12th:</b>
+					<br>
+					2015-16<br>
+					92.8%<br>
+					Dr.A.L.Ishrat Memorial Sunbeam School,Varanasi
+					<br>
+					<b>10th:</b>
+					<br>
+					2012-13<br>
+					10 CGPA<br>
+					Dr.A.L.Ishrat Memorial Sunbeam School,Varanasi
+					
+				</p>
+				<h3><b><u>Skills:</u></b></h3>			
+
+				<p>
+					<b>Web Designing:</b>
+					<br>
+					HTML<br>
+					CSS<br>
+					BOOTSTRAP<br>
+					JAVASCRIPT<br><br>
+					<b>Web Development:</b>
+					<br>
+					
+					PHP(Beginner)<br>
+					
+					<br>
+					<b>Other Programming Languages:</b>
+					<br>
+					C<br>
+					Java<br>
+					Python(beginner)<br>
+					</p>
+				</div>
+				<div class="col-md-6" style="text-align: justify;border-left: 2px solid grey">
+					<h3><b><u>Interests</u></b></h3>
+					Watching Movies,TV Series<br>
+					Playing Musical Instruments<br>
+					Playing Cricket<br>
+					<br><br>
+
+
+
+				</div>
+			</div>
+		</div>
+	<br><br><br>
+		<div class="card" style="background-color: #424242;color: white;">
+			<div class="row">
+			<div class="col-md-3" style="text-align: justify;"><h2>Blog Posts</h2><span style="margin-left: 0%"><i>from:-www.yashsinghblog.wordpress.com + other Sources</i></span></div>
+			<div class="col-md-9" style="border-left: 2px solid white">
+			<br><br><br><br><br><br>
+			</div>
+		</div>
+	</div>
+	<br><br>
+	<div class="card">
+			<h2>Get In Touch</h2>
+			<div style="text-align: justify;">
+				<hr style="border: none;height: 2px;color:grey;background-color:grey;">
+				<form>
+					
+					<input type="text" name="Name" class="form-control" placeholder="Name">
+					<br>
+					<input type="text" name="Name" class="form-control" placeholder="E-mail">
+					<br>
+					<textarea class="form-control" rows="10" placeholder="Message"></textarea> 
+					<br>
+					<center><button type="button" class="butn">Send Message</button></center>		
+				</form>
+			</div>
+	</div>
+	<br><br>
+	<div class="card row" style="background-color: #424242;color: white">
+		<h2>Personal Details</h2>
+
+	<hr style="border: none;height: 2px;color:grey;background-color:white;">	
+		<div class="col-md-4 ">
+			<div class="col-md-11 card" style="box-shadow: none; background-color: #424242;">
+				
+				<div class="img-responsive"><i class="material-icons" style="font-size: 80px;color: white">phone_iphone</i></div>
+				 <br>
+				(+91) 9532250224
+				<br>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+		
+		<div class="col-md-4 ">
+			<div class="col-md-11 card" style="box-shadow: none;background-color: #424242;">
+				<br>
+				<i class="material-icons" style="font-size: 80px;color: white">drafts</i>
+				<br>
+				yash.16bcs1104@abes.ac.in 
+				<br><br>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+		<div class="col-md-4 ">
+			<div class="col-md-11 card" style="box-shadow: none;background-color: #424242;">
+				<br>
+				<i class="material-icons" style="font-size: 80px;color: white">location_on</i>
+				<br>
+				ABES Engineering College,Ghaziabad
+				<br>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+	</div>
+	
+		<br><br><br>
+	</div>
+	<div class="col-md-1 Sidebar"> &nbsp</div>
+</div>
+<div class="container-fluid" style="background-color: black;color: white;text-align: center;"><br><br>Developed by:-Yash<br><br></div>
+</body>
+</html>
---
+++ portfolio/script.js
@@ -0,0 +1,105 @@
+var txt="Dreamer";
+var txt1="Designer";
+var txt2="Developer";
+var txt3="Learner";
+			var speed=150;
+			var i=0;
+			var j=txt.length;
+			var z=100;
+window.addEventListener('load', function() {
+
+		
+		
+	
+	
+	
+		typeWriter();
+		
+		
+
+});
+function typeWriter() {
+  		if (i < txt.length) {
+	    document.getElementById("move1").innerHTML += txt.charAt(i);
+    		i++;
+        	setTimeout(typeWriter, speed);
+		  }
+		else {
+			j=txt.length;
+			setTimeout(erase, z);
+		}
+		}
+
+function erase(){
+			if(j>=0){
+				document.getElementById("move1").innerHTML=txt.substr(0,j--);
+				setTimeout(erase, z);
+			}
+			else {i=0;
+				setTimeout(typeWriter1, z);
+			}
+		}
+function typeWriter1() {
+  		if (i < txt1.length) {
+	    document.getElementById("move1").innerHTML += txt1.charAt(i);
+    		i++;
+        	setTimeout(typeWriter1, speed);
+		  }
+		else {
+			j=txt1.length;
+			setTimeout(erase1, z);
+		}
+		}
+
+function erase1(){
+			if(j>=0){
+				document.getElementById("move1").innerHTML=txt1.substr(0,j--);
+				setTimeout(erase1, z);
+			}
+			else {i=0;
+				setTimeout(typeWriter2, z);
+			}
+		}
+function typeWriter2() {
+  		if (i < txt2.length) {
+	    document.getElementById("move1").innerHTML += txt2.charAt(i);
+    		i++;
+        	setTimeout(typeWriter2, speed);
+		  }
+		else {
+			j=txt2.length;
+			setTimeout(erase2, z);
+		}
+		}
+
+function erase2(){
+			if(j>=0){
+				document.getElementById("move1").innerHTML=txt2.substr(0,j--);
+				setTimeout(erase2, z);
+			}
+			else {i=0;
+				setTimeout(typeWriter3, z);
+			}
+		}
+function typeWriter3() {
+  		if (i < txt3.length) {
+	    document.getElementById("move1").innerHTML += txt3.charAt(i);
+    		i++;
+        	setTimeout(typeWriter3, speed);
+		  }
+		else {
+			j=txt3.length;
+			setTimeout(erase3, z);
+		}
+		}
+
+function erase3(){
+			if(j>=0){
+				document.getElementById("move1").innerHTML=txt3.substr(0,j--);
+				setTimeout(erase3, z);
+			}
+			else {i=0;
+				setTimeout(typeWriter, z);
+			}
+		}
+
