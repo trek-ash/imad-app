04befe2dbe1b2d9160aa6bc9a0f1d308acb55c27
Yash <31254076+trek-ash@users.noreply.github.com>
2018-02-16 00:19:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/c.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'c.jpg'));
 });
 
 
