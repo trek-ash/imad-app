ad5d4b54e7b6524ce0b759cbe11906cba0d829a7
Yash <31254076+trek-ash@users.noreply.github.com>
2018-02-16 02:20:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.get('/portfolio/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'portfolio', 'style.css'));
 });
 
-<<<<<<< HEAD
 app.get('/portfolio/script.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'portfolio', 'script.js'));
 });
@@ -23,17 +22,13 @@ app.get('/portfolio/pic1.jpg',function(req,res){
 });
 app.get('/portfolio/yash.jpg',function(req,res){
  res.sendFile(path.join(__dirname,'portfolio','yash.jpg'));
-=======
-app.get('/ui/c.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'c.jpg'));
->>>>>>> fbc3c34118b46e5ad082d1db52be3128edc038d1
 });
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8030;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
